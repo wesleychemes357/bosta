5c6627fb34eefd9cf0e325f1c0a4b88e87f41088
Ronaldo <150154326+wesleychemes357@users.noreply.github.com>
2025-12-11 15:07:49 -0300

    Update API endpoint for fetching noticias
    trocando a api do index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
 export async function getServerSideProps() {
   // Fetch data from  API notícias
-  const res = await fetch('bosta-xi.vercel.app/api/noticias/apinoticias')
+  const res = await fetch('https://paulo-api-6b.vercel.app/noticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
